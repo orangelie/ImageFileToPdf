4fdd4437a21dce6c0c11c6ac79e50b2830787b26
SANG WO PARK <88919358+orangelie@users.noreply.github.com>
2024-04-14 16:49:46 +0900

    Create .gitignore

---
+++ PngToPdf/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace PngToPdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("굴림", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Location = new System.Drawing.Point(148, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 103);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "찾기";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(12, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(776, 319);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(404, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 103);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "PDF변환!";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "le";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PngToPdf/Form1.cs
@@ -0,0 +1,73 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PngToPdf
+{
+    public partial class Form1 : Form
+    {
+        List<string> lstFiles = new List<string>();
+
+        public Form1() { InitializeComponent(); }
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        public void ImagesToPdf(List<string> imagepaths)
+        {
+            iTextSharp.text.Rectangle pageSize = null;
+
+            using (var srcImage = new Bitmap(imagepaths[0].ToString()))
+            {
+                pageSize = new iTextSharp.text.Rectangle(0, 0, srcImage.Width, srcImage.Height);
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                var document = new iTextSharp.text.Document(pageSize, 0, 0, 0, 0);
+                PdfWriter.GetInstance(document, ms).SetFullCompression();
+                document.Open();
+
+                foreach (string path in imagepaths)
+                {
+                    var image = iTextSharp.text.Image.GetInstance(path.ToString());
+                    document.Add(image);
+                }
+
+                document.Close();
+
+                File.WriteAllBytes("output.pdf", ms.ToArray());
+
+                MessageBox.Show("PDF 변환 성공 !");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlgOpen = new OpenFileDialog())
+            {
+                dlgOpen.Filter = "JPG File|*.jpg|PNG File|*.png";
+                dlgOpen.Title = "Select Audio File";
+                dlgOpen.Multiselect = true; 
+
+                if (dlgOpen.ShowDialog() == DialogResult.OK)
+                {
+                    lstFiles.Clear();
+                    listBox1.Items.Clear();
+
+                    for (int i = 0; i < dlgOpen.FileNames.Length; i++)
+                    {
+                        lstFiles.Add(dlgOpen.FileNames[i]);
+                        listBox1.Items.Add(dlgOpen.FileNames[i]);
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ImagesToPdf(lstFiles);
+        }
+    }
+}
