94eb463c5951cef7ac5c53d285bdcabafa4ef8f1
SANG WO PARK <88919358+orangelie@users.noreply.github.com>
2024-04-15 17:10:03 +0900

    Bug Fixed
    Bug Fixed

--- PngToPdf/Form1.Designer.cs
+++ PngToPdf/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -38,7 +39,7 @@
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.Font = new System.Drawing.Font("굴림", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button1.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.button1.Location = new System.Drawing.Point(148, 12);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(250, 103);
             this.button1.TabIndex = 0;
@@ -55,13 +56,16 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(776, 319);
             this.listBox1.TabIndex = 1;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button2.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.button2.Location = new System.Drawing.Point(404, 12);
+            this.button2.Location = new System.Drawing.Point(268, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(250, 103);
             this.button2.TabIndex = 2;
@@ -69,17 +73,32 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(524, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 103);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "초기화";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "le";
+            this.Text = "PDF변환 프로그램";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
             this.ResumeLayout(false);
 
         }
@@ -88,6 +107,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- PngToPdf/Form1.cs
+++ PngToPdf/Form1.cs
@@ -1,4 +1,5 @@
-﻿using iTextSharp.text.pdf;
+﻿using com.itextpdf.text.pdf;
+using iTextSharp.text.pdf;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -9,6 +10,7 @@ namespace PngToPdf
 {
     public partial class Form1 : Form
     {
+        int idx = -1;
         List<string> lstFiles = new List<string>();
 
         public Form1() { InitializeComponent(); }
@@ -37,7 +39,7 @@ namespace PngToPdf
 
                 document.Close();
 
-                File.WriteAllBytes("output.pdf", ms.ToArray());
+                File.WriteAllBytes("Output.pdf", ms.ToArray());
 
                 MessageBox.Show("PDF 변환 성공 !");
             }
@@ -47,15 +49,13 @@ namespace PngToPdf
         {
             using (OpenFileDialog dlgOpen = new OpenFileDialog())
             {
-                dlgOpen.Filter = "JPG File|*.jpg|PNG File|*.png";
-                dlgOpen.Title = "Select Audio File";
-                dlgOpen.Multiselect = true; 
+                dlgOpen.Filter = "JPG File|*.jpg|PNG File|*.png|All Files|*.*";
+                dlgOpen.Title = "Select File";
+                dlgOpen.Multiselect = true;
+                dlgOpen.RestoreDirectory = true;
 
                 if (dlgOpen.ShowDialog() == DialogResult.OK)
                 {
-                    lstFiles.Clear();
-                    listBox1.Items.Clear();
-
                     for (int i = 0; i < dlgOpen.FileNames.Length; i++)
                     {
                         lstFiles.Add(dlgOpen.FileNames[i]);
@@ -68,6 +68,107 @@ namespace PngToPdf
         private void button2_Click(object sender, EventArgs e)
         {
             ImagesToPdf(lstFiles);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lstFiles.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+
+            idx = listBox1.SelectedIndex;
+        }
+
+        private void Swap(int i1, int i2)
+        {
+            var temp = listBox1.Items[i1];
+            listBox1.Items[i1] = listBox1.Items[i2];
+            listBox1.Items[i2] = temp;
+
+            var t = lstFiles[i1];
+            lstFiles[i1] = lstFiles[i2];
+            lstFiles[i2] = t;
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                if (lstFiles.Count - 1 <= idx)
+                    return;
+
+                Swap(idx, idx + 1);
+                idx++;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                if (0 >= idx)
+                    return;
+
+                Swap(idx, idx - 1);
+                idx--;
+            }
+            else if (e.KeyCode == Keys.Return)
+            {
+                if (listBox1.SelectedIndex == -1)
+                    return;
+
+                using (Form form = new Form())
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.Dock = DockStyle.Fill;
+                    pictureBox.Image = Image.FromFile(lstFiles[listBox1.SelectedIndex]);
+
+                    form.Size = pictureBox.Image.Size;
+
+                    form.Controls.Add(pictureBox);
+                    form.ShowDialog();
+                }
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (idx == -1)
+                return;
+
+            using (Form form = new Form())
+            {
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Dock = DockStyle.Fill;
+                pictureBox.Image = Image.FromFile(lstFiles[listBox1.SelectedIndex]);
+
+                form.Size = pictureBox.Image.Size;
+
+                form.Controls.Add(pictureBox);
+                form.ShowDialog();
+            }
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            using (Form form = new Form())
+            {
+                form.Text = "사용법";
+                form.Size = new Size(400, 100);
+
+                Label lbl = new Label();
+                lbl.Dock = DockStyle.Fill;
+                lbl.Text =
+                    "[ Ctrl ] > 사용방법 창\n" +
+                    "[ ↓ ] > 아래파일과 순서 바꾸기\n" +
+                    "[ ↑ ] > 위파일과 순서 바꾸기\n" +
+                    "[ Enter ] 또는 [ 마우스 더블클릭 ] > 현재 선택된 파일 사진 띄우기\n" +
+                    "[ 마우스 클릭 ] > 파일 선택";
+
+                form.Controls.Add(lbl);
+                form.ShowDialog();
+            }
         }
     }
 }
